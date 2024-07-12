a2446c5bcec9f7ab920ca684e402750496123542
oalt <oa@lacheweg21.de>
2024-07-12 17:19:15 +0200

    Initial source commit

---
+++ src/MDD4All.DevOpsObserver.DataProviders.Contracts/IDevOpsStatusProvider.cs
@@ -0,0 +1,11 @@
+﻿using MDD4All.DevOpsObserver.DataModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDD4All.DevOpsObserver.DataProviders.Contracts
+{
+    public interface IDevOpsStatusProvider
+    {
+        Task<List<DevOpsStatusInformation>> GetDevOpsStatusListAsync(DevOpsSystem devOpsSystem);
+    }
+}
